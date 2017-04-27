ba5bb267c0d840da2625cd0a4f5beb6481c3efd8
Rudhi- <anirud3@vt.edu>
2017-04-27 15:45:29 -0400

    Some Work

--- app/src/main/java/com/markwiggans/vtscheduler/data/ScheduleGenerator.java
+++ app/src/main/java/com/markwiggans/vtscheduler/data/ScheduleGenerator.java
@@ -1,8 +1,38 @@
 package com.markwiggans.vtscheduler.data;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
+
 /**
  * Created by Anirudha Simha on 4/25/2017.
  */
 
 public class ScheduleGenerator {
+   public ArrayList<?> perm(ArrayList<?> courseAList, ArrayList<?> courseBList){
+       // creating empty list
+       ArrayList<Course> permuatations = new ArrayList<>();
+       permuatations.clear();
+       ArrayList<Course> mergedList = new ArrayList<>();
+
+
+
+       for(Object elementA : courseAList){
+           for(Object elementB : courseBList){
+               mergedList.clear();
+
+               if(courseAList.get(0) instanceof  ArrayList<?>){
+                   mergedList.addAll((ArrayList<Course>)elementA);
+                   mergedList.addAll((ArrayList<Course>)elementB);
+               }
+               else{
+                   mergedList.add((Course)elementA);
+                   mergedList.add((Course)elementB);
+               }
+           }
+       }
+
+
+        return new ArrayList<>();
+   }
 }
