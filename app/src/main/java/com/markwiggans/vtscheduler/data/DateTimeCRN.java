2e2762fbd6fcd10a9fe549bad5b940784f9c0cc0
Rudhi- <anirud3@vt.edu>
2017-05-02 17:15:58 -0400

    Acually staging all code changes for previous commit this time .... -_-

--- app/src/main/java/com/markwiggans/vtscheduler/data/DateTimeCRN.java
+++ app/src/main/java/com/markwiggans/vtscheduler/data/DateTimeCRN.java
@@ -4,5 +4,35 @@ package com.markwiggans.vtscheduler.data;
  * Created by Anirudha Simha on 5/2/2017.
  */
 
-public class DateTimeCRN {
+import android.support.annotation.NonNull;
+
+import java.util.ArrayList;
+
+/**
+ * Helper class to simply be used to merge meeting times fof crnsinto easy to manage form
+ */
+public class DateTimeCRN /*implements Comparable<DateTimeCRN>*/{
+
+    private int CRN;
+    private int startTime;
+    private int endTime;
+    public DateTimeCRN(int CRN, int startTime, int endTime ){
+        this.CRN = CRN;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    public int getCRN(){
+        return this.CRN;
+    }
+
+    public int getStartTime(){
+        return this.startTime;
+    }
+
+    public int getEndTime(){
+        return this.endTime;
+    }
+
+
 }
--- app/src/main/java/com/markwiggans/vtscheduler/data/ScheduleGenerator.java
+++ app/src/main/java/com/markwiggans/vtscheduler/data/ScheduleGenerator.java
@@ -2,6 +2,8 @@ package com.markwiggans.vtscheduler.data;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Objects;
 
 /**
@@ -9,36 +11,125 @@ import java.util.Objects;
  */
 
 public class ScheduleGenerator {
-   public ArrayList<?> perm(ArrayList<?> courseAList, ArrayList<?> courseBList){
-       // creating empty list
-       ArrayList<Course> permuatations = new ArrayList<>();
-       permuatations.clear();
-       ArrayList<Course> mergedList = new ArrayList<>();
 
+    /**
+     *
+     * @param mList list of objects to check collsions for
+     * @return true if collision detected false otherwise
+     */
+    public  boolean meetingTimesCollision(ArrayList<DateTimeCRN> mList){
+
+        if(mList.size() <= 1){
+            return false;
+        }
+
+        // sorting the list first
+        Collections.sort(mList, new Comparator<DateTimeCRN>() {
+            @Override
+            public int compare(DateTimeCRN p1, DateTimeCRN p2) {
+                return p1.getStartTime() - p2.getStartTime(); // Ascending
+            }
+        });
+
+        for(int i = 0; i < mList.size(); i++){
+            boolean startTimeColl = false;
+            boolean endTimeColl = false;
+            // first element
+            if (i == 0) {
+                startTimeColl = mList.get(i).getStartTime() >= mList.get(i+1).getStartTime() &&
+                        mList.get(i).getStartTime() <= mList.get(i+1).getStartTime();
+                endTimeColl = mList.get(i).getEndTime() >= mList.get(i+1).getEndTime() &&
+                        mList.get(i).getEndTime() <= mList.get(i+1).getEndTime();
+            }
+            // last element
+            else if(i+1 == mList.size()){
+                startTimeColl = (mList.get(i).getStartTime() >= mList.get(i-1).getStartTime() &&
+                        mList.get(i).getStartTime() <= mList.get(i-1).getStartTime());
+                endTimeColl = (mList.get(i).getEndTime() >= mList.get(i-1).getEndTime() &&
+                        mList.get(i).getEndTime() <= mList.get(i-1).getEndTime());
+            }
+            // every other element
+            else{
+                startTimeColl =
+                        // checking if interferance with one in front
+                        (mList.get(i).getStartTime() >= mList.get(i+1).getStartTime() &&
+                        mList.get(i).getStartTime() <= mList.get(i+1).getStartTime())||
+                        // checking if interferance with one in back
+                        (mList.get(i).getStartTime() >= mList.get(i-1).getStartTime() &&
+                                mList.get(i).getStartTime() <= mList.get(i-1).getStartTime());
+                endTimeColl =
+                        // checking if interferance with one in front
+                        (mList.get(i).getEndTime() >= mList.get(i+1).getEndTime() &&
+                        mList.get(i).getEndTime() <= mList.get(i+1).getEndTime()) ||
+                        // checking if interferance wit hone in back
+                        (mList.get(i).getEndTime() >= mList.get(i-1).getEndTime() &&
+                                mList.get(i).getEndTime() <= mList.get(i-1).getEndTime());
+            }
+
+            if(startTimeColl || endTimeColl){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+   public ArrayList<ArrayList<DateTimeCRN>> perm(ArrayList<?> courseAList, ArrayList<?> courseBList){
+       // creating empty list
+       ArrayList<ArrayList<DateTimeCRN>> permuatationsList = new ArrayList<>();
+       permuatationsList.clear();
+
+
+       ArrayList<DateTimeCRN> permutation = new ArrayList<>();
+        // creating every possible perm
        for(Object elementA : courseAList){
            for(Object elementB : courseBList){
-               mergedList.clear();
+               permutation.clear();
 
                if(courseAList.get(0) instanceof  ArrayList<?>){
-                   mergedList.addAll((ArrayList<Course>)elementA);
-                   mergedList.addAll((ArrayList<Course>)elementB);
+                   permutation.addAll((ArrayList<DateTimeCRN>)elementA);
+                   permutation.addAll((ArrayList<DateTimeCRN>)elementB);
                }
                else{
-                   mergedList.add((Course)elementA);
-                   mergedList.add((Course)elementB);
+                   permutation.add((DateTimeCRN) elementA);
+                   permutation.add((DateTimeCRN) elementB);
                }
-
+               // add the permutation to the list if it does not have any collisions
+                if(!meetingTimesCollision(permutation)){
+                    permuatationsList.add(permutation);
+                }
 
            }
        }
 
-        return new ArrayList<>();
+        return permuatationsList;
    }
 
-   public ArrayList<CRN> checkForConflicts(ArrayList<CRN> mergedList){
-       for(int i = 0; i < mergedList.size(); i++){
+    /**
+     *
+     * @param crnsList array list of arraylist, arrayslist are arraylist of DATeTime CRNS for each course
+     * @return final list pof permutations
+     */
+   public ArrayList<ArrayList<DateTimeCRN>> permperm(ArrayList<ArrayList<DateTimeCRN>> crnsList){
+       ArrayList<ArrayList<DateTimeCRN>> permutationsList = new ArrayList<>();
+       permutationsList.clear();
+
+       int numClasses = crnsList.size();
+       if (numClasses <= 1){
 
        }
-       return new ArrayList<CRN>();
+       else{
+           for(int i = 0; i < crnsList.size();){
+
+           }
+       }
+
+
+
+
+
+       return permutationsList;
    }
+
+
 }
