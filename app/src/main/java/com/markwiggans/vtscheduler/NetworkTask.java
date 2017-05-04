8ea1441280692ac570d14f9335da152c92a7e757
Anirudha Simha <anirud3@vt.edu>
2017-05-04 02:37:12 -0400

    POST works

--- app/src/main/java/com/markwiggans/vtscheduler/NetworkTask.java
+++ app/src/main/java/com/markwiggans/vtscheduler/NetworkTask.java
@@ -6,13 +6,16 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.markwiggans.vtscheduler.data.CRN;
 
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.HttpURLConnection;
@@ -50,7 +53,7 @@ public class NetworkTask extends AsyncTask<String, Void, String> {
         //String result = Constants.STATUS_OFFLINE;
         try {
             // Send the entered username and password to the server and check for success
-            result = attemptLogin();
+            attemptLogin();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -98,78 +101,87 @@ public class NetworkTask extends AsyncTask<String, Void, String> {
         InputStream is = null;
         String cookie = "empty cookie";
         String result="relogin";
+        HttpURLConnection conn;
 
         try {
             System.setProperty("http.keepAlive", "false");
-            Log.d("FitEx", "Creating connection to server for logging in.. url"+ context.getString(R.string.login_url));
-            HttpURLConnection conn;
-
-            JSONObject credentials = new JSONObject();
-
-
-
+            Log.d("Scheduler", "Creating connection to server for logging in.. url "+ context.getString(R.string.login_url));
+            HttpURLConnection urlConnection = null;
             if(post){
-                conn = (HttpURLConnection) ((new URL(
-                        context.getString(R.string.login_url)).openConnection()));
-                conn.setDoOutput(true);
-                conn.setRequestMethod("POST");
+
+                JSONObject credentials = new JSONObject();
                 credentials.put("semester", this.semester);
                 int[] crnsList = new int[this.crns.length];
                 for(int i = 0; i < this.crns.length; i++){
                     crnsList[i] = this.crns[i].getCRN();
                 }
-
                 credentials.put("unique_id", crnsList);
+
+
+
+                URL url = new URL("http://ec2-54-191-60-136.us-west-2.compute.amazonaws.com:8080/share");
+
+                conn = (HttpURLConnection) (url).openConnection();
+                conn.setDoOutput(true);
+                conn.setReadTimeout(READ_TIMEOUT_MS /* milliseconds */);
+                conn.setConnectTimeout(CONNECT_TIMEOUT_MS /* milliseconds */);
+
+                conn.setRequestProperty("Content-Type", "application/json");
+                conn.setRequestProperty("Accept", "application/json");
+                conn.setRequestMethod("POST");
+                conn.connect();
+                Log.d("Scheduler", "Sending schedule information to server... url "+ context.getString(R.string.login_url));
+                Writer osw = new OutputStreamWriter(conn.getOutputStream());
+                osw.write(credentials.toString());
+                osw.flush();
+                osw.close();
+                final int HttpResultCode = conn.getResponseCode();
+                is = HttpResultCode >= 400 ? conn.getErrorStream() : conn.getInputStream();
+
+                Log.d("FitEx", "Reposne is: " + HttpResultCode);
+                if (HttpResultCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader streamReader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                    StringBuilder responseStrBuilder = new StringBuilder();
+
+                    String inputStr;
+                    while ((inputStr = streamReader.readLine()) != null)
+                        responseStrBuilder.append(inputStr);
+                    JSONObject j = new JSONObject(responseStrBuilder.toString());
+
+                    Log.d("Scheduler",j.toString(4));
+
+                    Map<String, List<String>> headerFields = conn.getHeaderFields();
+                    //List<String> cookiesHeader = headerFields.get(context.getString(R.string.cookies_header));
+                    //cookie = cookiesHeader.get(0).substring(0, cookiesHeader.get(0).indexOf(";"));
+                    saveInSharedPreferences(cookie);
+                    //result =Constants.STATUS_LOGGED_IN;
+                } else if(HttpResultCode==401){
+                    Log.d("Scheduler", "Did not receive HTTP_OK from server!:"+401);
+                    //result=Constants.STATUS_RELOGIN;
+                }else{
+                    //result=Constants.STATUS_OFFLINE;
+                    Log.d("Scheduler", "Did not receive HTTP_OK from server!--other");
+
+                }
+
+
             }
             else{
-                conn = (HttpURLConnection) ((new URL(
-                        context.getString(R.string.login_url)+"/"+this.uuid).openConnection()));
-                conn.setRequestMethod("GET");
-            }
-
-
-            conn.setReadTimeout(READ_TIMEOUT_MS /* milliseconds */);
-            conn.setConnectTimeout(CONNECT_TIMEOUT_MS /* milliseconds */);
-
-            conn.setRequestProperty("Content-Type", "application/json");
-            conn.setRequestProperty("Accept", "application/json");
-
-            conn.connect();
-
-
-
-            Log.d("FitEx", "Sending login credentials to server... url"+ context.getString(R.string.login_url));
-            Writer osw = new OutputStreamWriter(conn.getOutputStream());
-            osw.write(credentials.toString());
-            osw.flush();
-            osw.close();
-
-            final int HttpResultCode = conn.getResponseCode();
-            is = HttpResultCode >= 400 ? conn.getErrorStream() : conn.getInputStream();
-
-            Log.d("FitEx", "Reposne is: " + HttpResultCode);
-            if (HttpResultCode == HttpURLConnection.HTTP_OK) {
-                Map<String, List<String>> headerFields = conn.getHeaderFields();
-                //List<String> cookiesHeader = headerFields.get(context.getString(R.string.cookies_header));
-                //cookie = cookiesHeader.get(0).substring(0, cookiesHeader.get(0).indexOf(";"));
-                //saveInSharedPreferences(cookie);
-                //result =Constants.STATUS_LOGGED_IN;
-            } else if(HttpResultCode==401){
-                Log.d("FitEx", "Did not receive HTTP_OK from server!:"+401);
-                //result=Constants.STATUS_RELOGIN;
-            }else{
-                //result=Constants.STATUS_OFFLINE;
-                Log.d("FitEx", "Did not receive HTTP_OK from server!--other");
+                URL url = new URL("http://ec2-54-191-60-136.us-west-2.compute.amazonaws.com:8080/share/"+this.uuid);
+                conn = (HttpURLConnection) (url).openConnection();
 
             }
+            
             conn.disconnect();
         } catch (Exception e) {
             e.printStackTrace();
 
         } finally {
+
             if (is != null) {
                 is.close();
             }
+
 
         }
 
